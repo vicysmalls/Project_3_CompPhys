47b9ab04863835f6448eed719963517d654ff198
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-03-12 17:06:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import random as rd
+from MatrixClass import*
 
 N = int(input("How big is the matrix (NxN): N = "))
-matrix = []
+matrix = MatrixClass(N)
 
-for i in range(N):
-    row = []
-    for j in range(N):
-        row.append(rd.randint(0,1))
-    matrix.append(row)
-
-for row in matrix:
-    print(row)
+matrix.show()
