4edf05ee6ee8bc13ee5a86646a6755224105c43f
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-03-27 10:18:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random as rd
 from MatrixClass import*
 
 N = int(input("How big is the matrix (NxN): N = "))
 matrix = MatrixClass(N)
-
 matrix.show()
+for times in range(0, 10000000):
+    row = rd.randint(0, N-1)
+    col = rd.randint(0, N-1)
+    matrix.checkStatus(row, col)
+matrix.show()
+plt.show()
