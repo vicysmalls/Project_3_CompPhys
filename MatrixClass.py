20ed2d63ba79ab6e265d394b176943ca9b988d42
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2024-04-03 14:45:46 -0400

    Update MatrixClass.py
    Added comments and fixed minor issue

--- MatrixClass.py
+++ MatrixClass.py
@@ -2,14 +2,17 @@ import random as rd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
+
 class MatrixClass:
     def __init__(self, N, amount):
+        # Constant Values
         self.amount = amount
         self.N = N
         self.J = 1
         self.k = 1.38E-23
         self.T = 200
         self.matrix = []
+        # Generate the Matrix
         for i in range(N):
             row = []
             for j in range(N):
@@ -18,7 +21,8 @@ class MatrixClass:
                     random_num = rd.randint(-1, 1)
                 row.append(random_num)
             self.matrix.append(row)
-
+            
+    # Displays the values in a matrix plot
     def show(self):
         cmap = mcolors.ListedColormap(['black', 'yellow'])
         # Plot the data
@@ -28,41 +32,53 @@ class MatrixClass:
         plt.title(f"Graph {self.amount}: T = {self.T}")
         self.amount += 1
 
-    # In order to know if the up,down,left,right is out of bounds,
-    # row or col equals N,or -1
+    # Returns: value of the cell above the input coordinates
+    # Parameters: row/col of coordinate desired
     def above(self, row, col):
         if row - 1 == -1:
             return 0
         else:
             return self.matrix[row-1][col]
-
+        
+    # Returns: value of the cell below the input coordinates
+    # Parameters: row/col of coordinate desired
     def below(self, row, col):
         if row + 1 == self.N:
             return 0
         else:
             return self.matrix[row+1][col]
-
+        
+    # Returns: value of the cell left of the input coordinates
+    # Parameters: row/col of coordinate desired
     def left(self, row, col):
         if col - 1 == -1:
             return 0
         else:
             return self.matrix[row][col-1]
 
+    # Returns: value of the cell right of the input coordinates
+    # Parameters: row/col of coordinate desired
     def right(self, row, col):
         if col + 1 == self.N:
             return 0
         else:
             return self.matrix[row][col+1]
 
+    # Calculates the change in energy after inverting value at input row/col
+    # Returns: change in energy after flip
+    # Parameters: row/col of coordinate desired
     def energyChange(self, row, col):
         total = self.above(row, col) + self.below(row, col) + self.right(row, col) + self.left(row, col)
-        H_old = self.matrix[row][col] * -self.J * total  # old energy
-        self.matrix[row][col] = -self.matrix[row][col]
-        H_new = self.matrix[row][col] * -self.J * total  # new energy
+        H_old = self.matrix[row][col] * -self.J * total  # energy before flip
+        H_new = -self.matrix[row][col] * -self.J * total  # energy after flip
         delE = H_new - H_old
         return delE
-
+    
+    # Determines if 'spin' should be accepted
+    # If change in energy is greater than zero or
+    # exp(-deltE / kT) is greater than a random value between 0 and 1,
+    # swap is enacted
     def checkStatus(self, row, col):
         delE = self.energyChange(row, col)
-        if delE >= 0 or np.random.rand() > np.exp(-delE/(self.k * self.T)):
+        if delE < 0 or np.random.rand() < np.exp(-delE/(self.k * self.T)):
             self.matrix[row][col] = -self.matrix[row][col]
