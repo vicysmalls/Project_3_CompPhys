956b0919c901834687e3903169d6b18d10c30b3a
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2024-04-03 15:00:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from MatrixClass import*
 import os
-amount = 1
+amount = 1        # amount of simulations
+N = 20            # size of the grid
+loops = 100000000 # iterations of simulation to do
+counter = 0       # used for showing progress of simulation
+percentage = 0    # used for showing progress of simulation
 
-N = int(input("How big is the matrix (NxN): N = "))
+# Create base Matrix
 matrix = MatrixClass(N, amount)
 matrix.show()
-loops = 1000000000
-counter = 0
-percentage = 0
-print(f"{percentage}%")
+
 for times in range(0, loops):
+    # Display a progress note while running (takes ~10 minutes)
     if counter == loops/100:
         percentage += 1
         counter = 0
         print(f"{percentage}%")
+    counter += 1
+    
+    # Pick a random coordinate within grid
     row = rd.randint(0, N-1)
     col = rd.randint(0, N-1)
+    # Determine if swap is necessary
     matrix.checkStatus(row, col)
-    counter += 1
+#Display the completed swap
 matrix.show()
 plt.show()
